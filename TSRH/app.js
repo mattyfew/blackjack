0dc6d75bb84fa0faa349367835ae176060e55162
Matt Fewer <mattfewer@hotmail.com>
2015-08-13 19:56:23 -0400

    new form is working, looking good

--- TSRH/app.js
+++ TSRH/app.js
@@ -13,14 +13,61 @@ app.use(methodOverride('_method'));
 
 app.use(express.static('public'));
 
+app.get('/tsrh/new', function(req,res){
+  
+  res.render('new.ejs');
+});
 
 
 
-
-app.get('/tsrh', function(req,res){
-  res.render('index.ejs');
+app.get('/tsrh/:id/edit', function(req,res){
+  var id = req.params.id;
+  db.get('SELECT * FROM threads WHERE id=?', id, function(err,rows){
+    if(err){
+      throw err;
+    } else {
+      res.render('edit.ejs', {threads:rows});
+    }
+  });
+});
+app.get('/tsrh/:id', function(req,res){
+//  var id = req.params.id;
+//  //Do I need this? admin only
+//  db.run('UPDATE threads SET title=?, syb')
+//  if(err){
+//    throw err;
+//  } else {
+//    res.redirect('/tsrh'+id)
+//  }
+});
+app.post('tsrh', function(req,res){
+//  db.run('INSERT INTO threads (title,subtitle,author_id,content,topic_img_id) VALUES (?,?,?,?,?)',
+//        req.body.)//need these values!
+//  if(err){
+//    throw err;
+//  } else {
+//    res.redirect('/tsrh');
+//  }
 })
-
+app.delete('/tsrh/:id', function(req,res){
+  //Not sure if I need this, only for admin
+  db.delete('SELECT * FROM threads', function(err,rows){
+    if (err){
+      throw errs;
+    } else {
+      res.redirect('/tsrh');
+    }
+  });
+});
+app.get('/tsrh', function(req,res){
+  db.all('SELECT * FROM threads', function(err,rows){
+    if(err){
+      throw err;
+    } else{
+      res.render('index.ejs'); //add {threads:rows}
+    }
+  })
+})
 app.get('/',function(req,res){
   res.redirect('/tsrh');
 });
